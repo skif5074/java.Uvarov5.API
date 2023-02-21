8cce718b427b1b162c258d8c906d1fd675363798
Aleksandr <skif5074@outlook.com>
2023-02-21 14:23:43 +0300

    ignor

---
+++ src/test/java/ru/netology/Post.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+
+public class Post {
+    public String name;
+    public String surname;
+    public String patrynamic;
+    public String phone;
+    public String passport;
+    public boolean subscription;
+    public FormDate birthday;
+
+}
